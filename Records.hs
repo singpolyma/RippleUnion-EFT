8d5518a455d2718a90c44303b68453ec1dbb0c7b
Stephen Paul Weber <singpolyma@singpolyma.net>
2014-03-16 13:27:26 -0500

    Snip a bunch of usused stuff

--- Records.hs
+++ Records.hs
@@ -6,17 +6,11 @@ import BasicPrelude
 import Text.Blaze.Html (Html)
 import Text.Blaze.Internal (MarkupM)
 import Network.URI (URI)
-import Text.Email.Validate (EmailAddress, validate)
 import Data.Base58Address (RippleAddress)
 import Data.Aeson (ToJSON(..), object, (.=))
 
 import Data.Text.Buildable
-import Database.SQLite.Simple (SQLData(SQLText), Connection)
-import Database.SQLite.Simple.FromRow (FromRow(..), field, fieldWith)
-import Database.SQLite.Simple.ToRow (ToRow(..))
-import Database.SQLite.Simple.ToField (ToField(..), toField)
-import Database.SQLite.Simple.FromField (fieldData, ResultError(ConversionFailed))
-import Database.SQLite.Simple.Ok (Ok(Ok, Errors))
+import Database.SQLite.Simple (Connection)
 import Text.Blaze.Html.Renderer.Text (renderHtmlBuilder)
 
 import qualified Ripple.Amount as Ripple
@@ -28,6 +22,9 @@ fee = 5
 limit :: Rational
 limit = 500
 
+s :: (IsString s) => String -> s
+s = fromString
+
 type Action a = URI -> Connection -> Vgg.Auth -> RippleAddress -> a
 
 instance Buildable (MarkupM a) where
@@ -36,94 +33,14 @@ instance Buildable (MarkupM a) where
 instance Buildable URI where
 	build = build . show
 
-instance ToRow Deposit where
-	toRow (Deposit rid fn email tel ripple amnt complete) =
-		[toField rid, toField fn, toField (show email), toField tel, toField (show ripple), toField amnt, toField complete]
-
-instance FromRow Deposit where
-	fromRow = Deposit <$> field <*> field <*> fieldWith emailF <*> field <*> fieldWith rippleF <*> field <*> field
-		where
-		emailF f = case fieldData f of
-			(SQLText t) -> case validate (encodeUtf8 t) of
-				Left e -> Errors [toException $ ConversionFailed "TEXT" "EmailAddress" e]
-				Right email -> Ok email
-			_ -> Errors [toException $ ConversionFailed "TEXT" "EmailAddress" "need a text"]
-
-		rippleF f = case fieldData f of
-			(SQLText t) -> case readMay t of
-				Nothing -> Errors [toException $ ConversionFailed "TEXT" "RippleAddress" "invalid"]
-				Just ripple -> Ok ripple
-			_ -> Errors [toException $ ConversionFailed "TEXT" "RippleAddress" "need a text"]
-
-instance (CanVerify a) => ToRow (Verification a) where
-	toRow (Verification item typ notes token) = [
-			toField itemId,
-			toField itemTable,
-			toField typ,
-			toField notes,
-			toField token
-		]
-		where
-		(itemId, itemTable) = verifyItemData item
-
-instance ToField VerificationType where
-	toField = toField . show
-
-class CanVerify a where
-	verifyItemData :: a -> (Int64, String)
-
-instance CanVerify Deposit where
-	verifyItemData d = (depositId d, "deposits")
-
-instance Eq Form where
-	(Form _ a1) == (Form _ a2) = a1 == a2
-
-data Home = Home {
-		renderedDepositForm :: [Form],
-		renderedQuoteForm   :: [Form]
-	}
-
 data Form = Form {
 		formHtml   :: Html,
 		formAction :: URI
 	}
 
-data DepositSuccess = DepositSuccess {
-		successfulDeposit :: [Deposit],
-		higherVerificationNeeded :: Bool,
-		renderedStripeVerifyForm :: [Form],
-		homeLink :: URI
-	}
+instance Eq Form where
+	(Form _ a1) == (Form _ a2) = a1 == a2
 
-data Deposit = Deposit {
-		depositId       :: Int64,
-		depositorFN     :: Text,
-		depositorEmail  :: EmailAddress,
-		depositorTel    :: Text,
-		depositorRipple :: RippleAddress,
-		depositAmount   :: Double,
-		depositComplete :: Bool
-	}
-
-data VerificationType = AutomatedPhoneVerification | ManualPhoneVerification | StripeVerification
-	deriving (Show, Read, Enum)
-
-data Verification a = Verification {
-		verificationItem :: a,
-		verificationType :: VerificationType,
-		verificationNotes :: Maybe Text,
-		verificationAddrToken :: Maybe Text
-	}
-
-data PlivoDeposit = PlivoDeposit {
-		plivoCode :: String
-	}
-
-data PlivoConfig = PlivoConfig {
-		plivoAuthId    :: String,
-		plivoAuthToken :: String,
-		plivoTel       :: String
-	}
 
 data Header = Header {
 	}
@@ -138,9 +55,7 @@ instance Eq Header where
 header :: Header
 header = Header
 
-s :: (IsString s) => String -> s
-s = fromString
-
+-- Federation stuff
 
 data Alias = Alias {
 		alias :: Text,
