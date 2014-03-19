712df685a22bd8859788e4e4ffdf00e7cd46a6a9
Stephen Paul Weber <singpolyma@singpolyma.net>
2014-03-19 18:16:28 -0500

    Automated payouts

--- payout.hs
+++ payout.hs
@@ -1,10 +1,15 @@
 module Main (main) where
 
-import Prelude ()
-import BasicPrelude
+import Prelude (read)
+import BasicPrelude hiding (getArgs, read)
 import Data.Fixed (Centi)
+import System.Environment (getArgs)
 import System.IO (hPutStrLn, stderr)
 import Database.SQLite.Simple (Connection, open, execute, execute_, query_)
+
+import qualified Vogogo as Vgg
+import qualified Vogogo.Account as Vgg
+import qualified Vogogo.Transaction as Vgg
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -22,8 +27,11 @@ s = fromString
 
 main :: IO ()
 main = do
-	[dbpth] <- getArgs
-	db <- open $ textToString dbpth
+	[dbpth,vuser,vkey,vtoken] <- getArgs
+	db <- open dbpth
+
+	let vogogo = Vgg.Auth vuser vkey vtoken
+	Right wallet <- Vgg.getWallet vogogo
 
 	transactions <- query_ db (s"SELECT txhash,vogogo_uuid,amount,paid_out FROM transactions INNER JOIN accounts ON transactions.dt = accounts.id WHERE currency='CAD' AND dt AND paid_out < amount ORDER BY ledger_index ASC")
 
@@ -37,6 +45,7 @@ main = do
 			execute db (s"UPDATE transactions SET paid_out = ? WHERE txhash = ?")
 				(realToFrac amount :: Double, txhash)
 
-		hPutStrLn stderr $ "SENDING PAYOUT OF " ++ textToString (show payout) ++ " TO " ++ textToString (show account)
+		hPutStrLn stderr $ "SENDING PAYOUT OF " ++ textToString (show payout) ++ " TO " ++ account
+		print =<< Vgg.sendEFT vogogo wallet (Vgg.UUID account) payout (read "CAD")
 
 		execute_ db (s"END TRANSACTION")
